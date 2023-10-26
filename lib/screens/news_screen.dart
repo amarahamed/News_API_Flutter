0342d2e5148bb2035b4d9e0a9b4b04ef586b3626
Amar Ahamed <amarahamed0615@gmail.com>
2023-10-26 11:05:40 -0400

    back button news screen

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -6,10 +6,10 @@ import 'package:news_api/utilities/utilities.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class NewsScreen extends StatelessWidget {
-  dynamic news;
-  String category;
+  final dynamic news;
+  final String category;
 
-  NewsScreen({super.key, this.news, required this.category});
+  const NewsScreen({super.key, this.news, required this.category});
 
   // launch url
   Future<void> launchURL() async {
@@ -27,89 +27,97 @@ class NewsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryBackgroundColor,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: kPrimaryBackgroundColor,
-                  image: DecorationImage(
-                      image: NetworkImage(news["image"]),
-                      fit: BoxFit.fitHeight,
-                      alignment: Alignment.topCenter),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CategoryTextBubble(category: category),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      ContainerBubble(
-                        childWidget: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Text(
-                              news["title"],
-                              style: kTitleTextTitle.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.normal,
-                                height: 1.3,
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              localTimeConvert(news["publishedAt"]),
-                              style: kSmallTitleTextStyle,
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Container(
+              decoration: BoxDecoration(
+                color: kPrimaryBackgroundColor,
+                image: DecorationImage(
+                    image: NetworkImage(news["image"]),
+                    fit: BoxFit.fitHeight,
+                    alignment: Alignment.topCenter),
               ),
-            ),
-            Expanded(
-              flex: 5,
               child: Padding(
-                padding: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(12.0),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 15),
-                          child: Text(
-                            news["source"]["name"],
-                            style: kTitleTextTitle,
-                          ),
-                        ),
-                        Text(
-                          news["content"],
-                          style: kBodyTextStyle,
-                        ),
-                      ],
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CategoryTextBubble(category: category),
+                    const SizedBox(
+                      height: 10,
                     ),
-                    TextButton(
-                        onPressed: () {}, child: const Text("Learn more")),
+                    ContainerBubble(
+                      childWidget: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            news["title"],
+                            style: kTitleTextTitle.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              height: 1.3,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            localTimeConvert(news["publishedAt"]),
+                            style: kSmallTitleTextStyle,
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: Text(
+                          news["source"]["name"],
+                          style: kTitleTextTitle,
+                        ),
+                      ),
+                      Text(
+                        news["content"],
+                        style: kBodyTextStyle,
+                      ),
+                    ],
+                  ),
+                  TextButton(onPressed: () {}, child: const Text("Learn more")),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
